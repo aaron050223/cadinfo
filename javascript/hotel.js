605a4114365a126ebd85cd4e87c1d5c2f5f0ef6d
aaron050223 <agalcan1410@g.educaand.es>
2025-03-10 20:31:05 +0100

    Commit 24 10/03/2025 - Añado dos JS

---
+++ javascript/hotel.js
@@ -0,0 +1,35 @@
+// Cambio nº2, Añado un hotel nuevo
+
+window.onload = function() {
+    // Seleccionamos el contenedor de los hoteles
+    const contenedorHoteles = document.querySelector(".carnavaleros_destacados");
+
+    // Creamos un nuevo artículo para el cuarto hotel
+    const nuevoHotel = document.createElement("article");
+    nuevoHotel.classList.add("carnavaleros_destacados");
+
+    // Creamos el contenedor de la imagen y el texto del hotel
+    const divContenedor = document.createElement("div");
+    divContenedor.classList.add("img_info_carnavaleros");
+
+    // Creamos la imagen del cuarto hotel
+    const nuevaImagen = document.createElement("img");
+    nuevaImagen.src = "assets/hotel_alquimia1.png";  // Ruta de la nueva imagen del hotel
+    nuevaImagen.alt = "Nuevo Hotel";
+    nuevaImagen.classList.add("foto_hotel");
+
+    // Creamos el nombre del cuarto hotel
+    const nombreHotel = document.createElement("p");
+    nombreHotel.classList.add("nombre_hotel_4");
+    nombreHotel.textContent = "Hotel Alquimia | 3 ⭐";
+
+    // Agregamos la imagen y el nombre del hotel al div contenedor
+    divContenedor.appendChild(nuevaImagen);
+    divContenedor.appendChild(nombreHotel);
+
+    // Agregamos el div contenedor al nuevo artículo
+    nuevoHotel.appendChild(divContenedor);
+
+    // Finalmente, agregamos el nuevo hotel al final del contenedor de hoteles
+    contenedorHoteles.appendChild(nuevoHotel);
+};
---
+++ javascript/sesion.js
@@ -0,0 +1,5 @@
+// Cambio nº1, cambio un "Input text" por un "Input password"
+
+window.onload = function() {
+    document.querySelector(".Introducir_contraseña").type = "password";
+};
